c79e9b565fa519b47c29839944536cc889d06534
André <andremgmonteiro01@gmail.com>
2024-12-21 22:43:04 +0000

    Implemented a few types and defines

--- Crinkly/Utility/Types.hpp
+++ Crinkly/Utility/Types.hpp
@@ -1,6 +1,14 @@
 #pragma once
 
+#define var auto
+
+using Bit = bool;
 using Byte = unsigned char;
 using Word = unsigned short;
 using Address = unsigned short;
 using Size = unsigned int;
+
+using U8 = unsigned char;
+using U16 = unsigned short;
+using U32 = unsigned int;
+using U64 = unsigned long long;
--- Crinkly/Utility/Utils.hpp
+++ Crinkly/Utility/Utils.hpp
@@ -3,7 +3,14 @@
 #include <string>
 #include <vector>
 
+#include "Types.hpp"
+
 [[noreturn]]
 void IncorrectUsage(const std::string& exeName);
 
 std::vector<std::string> SplitString(const std::string& str, const std::string& delimiter);
+
+static inline constexpr Size operator"" _Kb(unsigned long long n)
+{
+    return static_cast<Size>(n) * 1024;
+}
