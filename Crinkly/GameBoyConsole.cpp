9809b47058eb014c552580b772664cfd9993e22b
André <andremgmonteiro01@gmail.com>
2024-12-21 22:45:33 +0000

    Mixing the soup all together

--- Crinkly/GameBoyConsole.cpp
+++ Crinkly/GameBoyConsole.cpp
@@ -1,50 +1,38 @@
 #include "GameBoyConsole.hpp"
 
-#include <fstream>
 #include <iostream>
-
-#include "Utility/Types.hpp"
-#include "Utility/Utils.hpp"
 
 GameBoyConsole::GameBoyConsole()
 {
-    std::cout << "GameBoyConsole created.\n";
+    m_Bus = std::make_shared<Bus>();
+    m_CPU = std::make_shared<CPU>();
+
+    m_CPU->Register('A', 0x01);
+    m_CPU->Register('B', 0x00);
+    m_CPU->Register('C', 0x13);
+
+    std::cout << std::format("{:#04x}\n", m_CPU->Register('A'));
+    std::cout << std::format("{:#04x}\n", m_CPU->Register('B'));
+    std::cout << std::format("{:#04x}\n", m_CPU->Register('C'));
+
+    m_CPU->Register("AF", 0x0F2A);
+    m_CPU->Register("BC", 0xFF69);
+    m_CPU->Register("SP", 0x1234);
+
+    std::cout << std::format("{:#06x}\n", m_CPU->Register("AF"));
+    std::cout << std::format("{:#04x}\n", m_CPU->Register('B'));
+    std::cout << std::format("{:#04x}\n", m_CPU->Register("C"));
+    std::cout << std::format("{:#06x}\n", m_CPU->Register("SP"));
 }
 
 GameBoyConsole::~GameBoyConsole()
 {
     EjectCartridge();
-    std::cout << "GameBoyConsole destroyed.\n";
 }
 
 void GameBoyConsole::InsertCartridge(const std::string& cartridge)
 {
-    auto splitCartridge = SplitString(cartridge, "\\");
-    auto cartridgeName = splitCartridge[splitCartridge.size() - 1];
-    std::cout << "Inserting cartridge: " << cartridgeName << '\n';
-
-    std::ifstream input(cartridge, std::ios::binary);
-    
-    if(input.fail())
-    {
-        std::cerr << "Failed to open file: " << cartridgeName << '\n';
-        std::cin.get();
-        exit(127);
-    }
-
-    int i = 0;
-    while(!input.eof())
-    {
-        Byte byte;
-        input.read(reinterpret_cast<char*>(&byte), sizeof(Byte));
-        if (i >= 0x104 && i <= 0x133)
-        {
-            std::cout << std::hex << static_cast<int>(byte) << '\n';
-        }
-        i++;
-    }
-    
-    input.close();
+    // m_Cartridge = std::make_shared<Cartridge>(cartridge);
 }
 
 void GameBoyConsole::EjectCartridge()
--- Crinkly/GameBoyConsole.hpp
+++ Crinkly/GameBoyConsole.hpp
@@ -1,12 +1,38 @@
 #pragma once
+#include <memory>
 #include <string>
+
+#include "Hardware/Bus.hpp"
+#include "Hardware/Cartridge.hpp"
+#include "Hardware/CPU.hpp"
+#include "Utility/Utils.hpp"
+#include "Utility/Types.hpp"
 
 class GameBoyConsole
 {
+private: // Specifications
+    inline constexpr static double CPU_FREQUENCY = 4'194'304.0;
+    
+    inline constexpr static Size RESOLUTION_X = 160;
+    inline constexpr static Size RESOLUTION_Y = 144;
+
+    inline constexpr static Size MAX_SPRITES = 40;
+    inline constexpr static Size MAX_SPRITES_PER_LINE = 10;
+
 public:
     GameBoyConsole();
     ~GameBoyConsole();
+
+    GameBoyConsole(const GameBoyConsole&) = delete;
+    GameBoyConsole& operator=(const GameBoyConsole&) = delete;
+    GameBoyConsole(GameBoyConsole&&) = delete;
+    GameBoyConsole& operator=(GameBoyConsole&&) = delete;    
     
     void InsertCartridge(const std::string& cartridge);
     void EjectCartridge();
+
+private:
+    std::shared_ptr<CPU> m_CPU;
+    std::shared_ptr<Bus> m_Bus;
+    std::shared_ptr<Cartridge> m_Cartridge;
 };
